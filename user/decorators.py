9c9e8a6bbc4b672da9b25ec107a5f91671e047a1
Jordan <jxd311@student.bham.ac.uk>
2023-08-17 16:20:28 +0100

    committing work done so far to gitlab

--- user/decorators.py
+++ user/decorators.py
@@ -5,6 +5,8 @@ from django.contrib import messages
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
+            if request.user.username == "admin":
+                return redirect('admin:index')
             if request.user.role in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
